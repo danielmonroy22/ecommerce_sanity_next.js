42addbf1b2802d396d4de629e59164275c378969
danielmonroy22 <daniel.monroy90@gmail.com>
2022-11-24 22:10:17 -0800

    second commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
 import '../styles/globals.css'
-
+import { Layout } from '../components';
+import { StateContext } from '../context/StateContext';
+import { Toaster } from 'react-hot-toast';
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    // This is how we pass the data to every single component inside the braces
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+
+      </Layout>
+
+
+
+    </StateContext>
+
+  )
+
 }
 
 export default MyApp
